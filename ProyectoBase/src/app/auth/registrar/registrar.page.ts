14c321c90d48e26813a6454b16957718ea6ad0ca
Francisco Martinez <isa.franciscomtz@hotmail.com>
2019-09-28 09:00:52 -0500

    Practica login

--- ProyectoBase/src/app/auth/registrar/registrar.page.ts
+++ ProyectoBase/src/app/auth/registrar/registrar.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registrar',
@@ -6,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registrar.page.scss'],
 })
 export class RegistrarPage implements OnInit {
+  registraForm: FormGroup;
+  submitted = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) {
+    this.registraForm = this.formBuilder.group({
+        nombre: ['', Validators.required],
+        correo: ['', [Validators.required , Validators.email]],
+        contrasena: ['', [Validators.required, Validators.minLength(6)]],
+        confirmacionContrasena: ['', Validators.required]
 
-  ngOnInit() {
+    });
+  }
+
+  ngOnInit() {}
+
+  get f() { return this.registraForm.controls;}
+
+  registrar() {
+    console.log(this.registraForm.valid);
   }
 
 }
